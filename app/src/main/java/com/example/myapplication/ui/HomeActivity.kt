3fbd4478f6da6654244696beca72fa4335c022b8
akash <dragon.abhi8@gmail.com>
2022-01-17 19:58:17 +0530

    broadcast Receiver

--- app/src/main/java/com/example/myapplication/ui/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/HomeActivity.kt
@@ -9,19 +9,29 @@ import com.example.myapplication.R
 import com.example.myapplication.broadcast.BroadcastReceivers
 
 class HomeActivity : AppCompatActivity() {
+
+    private val intentFilter = IntentFilter("Sending Message")
+    private val broadcastReceivers = BroadcastReceivers()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val btn:Button = findViewById(R.id.btn)
+        val btn: Button = findViewById(R.id.btn)
 
         btn.setOnClickListener {
 
             this.sendBroadcast(Intent("Sending Message"))
 
         }
-        this.registerReceiver(BroadcastReceivers(), IntentFilter("Sending Message"))
-
+        this.registerReceiver(broadcastReceivers, intentFilter)
 
     }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(broadcastReceivers)
+    }
+
 }
