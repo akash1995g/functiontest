d5490e4cb915e2cea1775eb66df530fce2bb5a55
akash <dragon.abhi8@gmail.com>
2022-01-19 20:05:30 +0530

    added Service

--- app/src/main/java/com/example/myapplication/broadcast/BroadcastReceivers.kt
+++ app/src/main/java/com/example/myapplication/broadcast/BroadcastReceivers.kt
@@ -4,12 +4,19 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.core.app.JobIntentService.enqueueWork
+import com.example.myapplication.service.MyService
 
 private const val TAG = "BroadcastReceivers"
+
 class BroadcastReceivers : BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
         Log.d(TAG, "onReceive: ")
 
+        if (p0 != null) {
+            //MyService.enqueueWork(p0, Intent(p0, MyService::class.java))
+            enqueueWork(p0,MyService::class.java,2,Intent(p0,MyService::class.java))
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/myapplication/service/MyService.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication.service
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.JobIntentService
+import java.lang.Exception
+
+private const val TAG = "MyService"
+
+class MyService : JobIntentService() {
+
+    companion object {
+        fun enqueueWork(context: Context, intent: Intent) {
+            enqueueWork(context, MyService::class.java, 1, intent)
+
+        }
+    }
+
+    override fun onHandleWork(intent: Intent) {
+
+        Log.d(TAG, "onHandleWork: " + Thread.currentThread().name)
+        Thread(Runnable {
+            try {
+                Log.d(TAG, "onHandleWork: logging")
+                Thread.sleep(1_000)
+            } catch (e: Exception) {
+                Log.d(TAG, "onHandleWork: " + e.message)
+            }
+        }).start()
+
+    }
+
+}
