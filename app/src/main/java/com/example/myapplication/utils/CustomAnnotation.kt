aacd2dd863e7b4a0342bef2bf70cf3d9e492990a
akash <dragon.abhi8@gmail.com>
2022-02-03 08:41:29 +0530

    added annotation

---
+++ app/src/main/java/com/example/myapplication/utils/CustomAnnotation.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication.utils
+
+/*****************
+// in Java
+public @interface CustomAnnotation {
+
+}
+
+ ******************/
+
+annotation class CustomAnnotation(
+    val value1: Int,
+    val value2: String
+)
+
+
+
+
--- app/src/main/java/com/example/myapplication/view/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/view/HomeActivity.kt
@@ -3,10 +3,18 @@ package com.example.myapplication.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
+import com.example.myapplication.utils.CustomAnnotation
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        testFunction()
+    }
+
+    @CustomAnnotation(0, "String")
+    private fun testFunction() {
+
     }
 }
