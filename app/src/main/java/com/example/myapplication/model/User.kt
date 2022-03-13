5744dde8653fa4d79c82ba408d9ae8c6001133cd
akash <dragon.abhi8@gmail.com>
2022-03-13 15:54:07 +0530

    added Data binding to app

--- app/src/main/java/com/example/myapplication/model/User.kt
+++ app/src/main/java/com/example/myapplication/model/User.kt
@@ -1,3 +1,7 @@
 package com.example.myapplication.model
 
-data class User(val userName:String, val age:Int)
+data class User(
+    val userName: String = "Image",
+    val age: Int = 25,
+    var url: String = "https://source.unsplash.com/user/c_v_r/1900x800"
+)
--- app/src/main/java/com/example/myapplication/view/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/view/HomeActivity.kt
@@ -1,12 +1,21 @@
 package com.example.myapplication.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityHomeBinding
+import com.example.myapplication.model.User
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        val bind = DataBindingUtil
+            .setContentView<ActivityHomeBinding>(this, R.layout.activity_home)
+        setContentView(bind.root)
+
+        bind.setImageName(User())
+
     }
 }
